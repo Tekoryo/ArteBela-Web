e6db62391a25e3b48e54af0252ee993aa2c1227f
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-07-22 23:18:33 -0300

    w

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
+var rollAtual = 0;
 $('#ante, #prox').click(fazerScroll);
 
 function fazerScroll(e) {
-    var direcao = e.target.id == 'frente' ? 1 : -1;
+    var direcao = e.target.id == 'prox' ? 1 : -1;
     $('#fotos').animate({
         scrollLeft: scrollAtual += 200 * direcao
     }, 1000);
