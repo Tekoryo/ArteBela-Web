9fce8a986cdc438a5122090353056a4891d5bfd3
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-07-28 20:22:18 -0300

    js - execluido

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 
-$(document).ready(function image(){
-
-  $("#fotos1").click(function image(){
-    if($(this).attr("src") == "imagens/fotos/im4.jpg") 
-      $(this).attr("src","imagens/fotos/im5.jpg");
-    else
-      $(this).attr("src","imagens/fotos/im2.jpg");
-
-  });
-});
