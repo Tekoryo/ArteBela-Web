6371755eda94acc2a8775af8e80c6b7c45d20de9
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-09-25 22:31:04 -0300

    Sr Scroll suave OK

--- _code/script.js
+++ _code/script.js
@@ -4,10 +4,14 @@ menuitem.forEach(item=>{
     item.addEventListener('click',scrollToIdOnClick)
 })
 function scrollToIdOnClick(event){
+    event.preventDefault()
     const element = event.target;
     const id = element.getAttribute('href')
-    const section = document.querySelector(id)
-    console.log(id)
+    const to = document.querySelector(id).offsetTop
+   window.scroll({
+       top:to - 80,
+       behavior: "smooth",
+   })
 
 }
 /*Banner*/
