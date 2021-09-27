3aa8750c2cb55e502e4ad7c03927c9ebb5605d05
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-09-27 12:08:33 -0300

    funcionalidade da galeria

--- _code/script.js
+++ _code/script.js
@@ -33,8 +33,26 @@ function nextImage(){
 }    
 function start(){
     setInterval(()=>{
-        // troca de imagem
+         // troca de imagem
         nextImage()
     },time)
 }
 window.addEventListener("load",start)
+/* Galeria*/
+let imagens=document.querySelectorAll('.small_img')
+let modal=document.querySelector('.modal')
+let modalImg=document.querySelector('#modal_img')
+let btClose=document.querySelector('#bt_close')
+let srcVal="";
+
+for(let i=0;i<imagens.length;i++){
+    imagens[i].addEventListener('click',function(){
+
+        srcVal=imagens[i].getAttribute('src')
+        modalImg.setAttribute('src',srcVal)
+        modal.classList.toggle('modal_active')
+    })
+}
+btClose.addEventListener('click',function(){
+    modal.classList.toggle('modal_active')
+})
