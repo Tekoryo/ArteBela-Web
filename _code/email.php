ea76f5920ce9e214e0fef51e675264405d3c312f
Thiago C. Santana <thiagocoelho.coelho2@gmail.com>
2021-09-30 14:48:32 -0300

    completo so falta testar

--- _code/email.php
+++ _code/email.php
@@ -1,6 +1,24 @@
 <?php
+if(isset($_POST['email'])&& !empty(addslashes(($_POST['email'])))){
 $nome = addslashes(($_POST['name']));
 $email = addslashes(($_POST['email']));
 $mensagem = addslashes(($_POST['mensagem']));
 
-?>
+$to = "thiagocoelho.coelho2@gmail.com";
+$subject = "Contato - ArtBela Site";
+$body = "Nome: ".$nome. "/r/n".
+        "Email: ".$email."/r/n".
+        "Mensagem: ".$mensagem;
+$header="From:contato@artebela.com"."/r/n".
+        "Reply-To:".$email."/r/n".
+        "X=Mailer:PHP/".phpversion();
+        
+if(mail($to,$subject,$body,$header)){
+    echo("Enviado com sucesso!");
+}else{
+    echo("Falha ao enviar!");
+}
+
+}
+
+?> 
